b9e4c7e6e00614e9505b936dfad27eafc45090b3
RahidAmin <rahidamin19@gmail.com>
2024-07-01 08:23:37 +0600

    weather card added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Temperature from './Temperature'
 
 const App = () => {
     return (
         <div>
-
+            <Temperature></Temperature>
         </div>
     )
 }
